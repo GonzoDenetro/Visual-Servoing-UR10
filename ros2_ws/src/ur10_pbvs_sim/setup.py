5ae94bc0e090201c5986ddf53db6e94f6ad9a8c1
Gonzo Denetro <gonzi.ceron.denetro@gmail.com>
2026-05-06 12:43:27 -0600

    Simulation resources added to setup

--- ros2_ws/src/ur10_pbvs_sim/setup.py
+++ ros2_ws/src/ur10_pbvs_sim/setup.py
@@ -1,6 +1,15 @@
 from setuptools import find_packages, setup
+import os
 
 package_name = 'ur10_pbvs_sim'
+
+def package_files(directory):
+    paths = []
+    for (path, _, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join(path, filename))
+    return paths
+
 
 setup(
     name=package_name,
@@ -10,6 +19,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        
+        #Simulation Directories
+        (os.path.join('share', package_name, 'launch'), package_files('launch')),
+        (os.path.join('share', package_name, 'urdf'), package_files('urdf')),
+        (os.path.join('share', package_name, 'config'), package_files('config')),
+        (os.path.join('share', package_name, 'meshes'), package_files('meshes')),
+        (os.path.join('share', package_name, 'worlds'), package_files('worlds')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
