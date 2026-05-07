07736fd1c2b7a1468d8f43318b6f50211eacdc04
Gonzo Denetro <gonzi.ceron.denetro@gmail.com>
2026-05-07 09:06:26 -0600

    Camera added to URDF

--- ros2_ws/src/ur10_pbvs_sim/launch/spawn_ur.launch.py
+++ ros2_ws/src/ur10_pbvs_sim/launch/spawn_ur.launch.py
@@ -10,6 +10,7 @@ from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 
@@ -80,7 +81,13 @@ def launch_setup(context, *args, **kwargs):
             initial_positions_file_abs,
         ]
     )
-    robot_description = {"robot_description": robot_description_content}
+    robot_description = {
+        #"robot_description": robot_description_content
+        "robot_description": ParameterValue(
+        robot_description_content,
+        value_type=str
+        )
+    }
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
